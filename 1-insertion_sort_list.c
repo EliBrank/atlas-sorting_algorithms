e73cd1b6dad0a365d93b3575df95ecd52fa4d4fa
EliBrank <eliasbrinkman@gmail.com>
2024-04-06 12:47:25 -0500

    create insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,5 +6,35 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *tmp = NULL;
+	listint_t *swap = NULL;
+	listint_t *current = NULL;
 
+	if (*list == NULL || (*list)->next == NULL)
+		return;
+
+	current = *list;
+	
+	while (current != NULL)
+	{
+		tmp = current;
+		while (tmp->prev != NULL && tmp->prev->n > tmp->n)
+		{
+			swap = tmp->prev;
+			tmp->prev = swap->prev;
+			swap->next = tmp->next;
+			if (swap->prev != NULL)
+				swap->prev->next = tmp;
+			if (tmp->next !=NULL)
+				tmp->next->prev = swap;
+			tmp->next = swap;
+			swap->prev = tmp;
+			if (current == tmp)
+				current = swap;
+			while ((*list)->prev != NULL)
+				*list = (*list)->prev;
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }

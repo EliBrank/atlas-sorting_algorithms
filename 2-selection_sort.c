1968dc09a11c1f0c7b8c37d4853c9a020b7bcf76
EliBrank <eliasbrinkman@gmail.com>
2024-04-06 14:46:53 -0500

    create, debug selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,5 +7,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	int tmp;
+	size_t i, j, min_index;
+	bool swap_made;
 
+	for (i = 0; i < (size - 1); i++)
+	{
+		swap_made = false;
+		min_index = i;
+		for (j = (i + 1); j < size; j++)
+		{
+			if (array[j] < array[min_index])
+			{
+				swap_made = true;
+				min_index = j;
+			}
+		}
+		tmp = array[i];
+		array[i] = array[min_index];
+		array[min_index] = tmp;
+		if (swap_made == true)
+			print_array(array, size);
+	}
 }

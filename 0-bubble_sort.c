b07023f82c31cbd15913bfc78405dd9affde960f
EliBrank <eliasbrinkman@gmail.com>
2024-04-06 10:30:55 -0500

    implement bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,5 +7,22 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	int i, tmp;
+	bool is_sorted = false;
 
+	while (is_sorted == false)
+	{
+		for (i = 0; i < (size - 1); i++)
+		{
+			is_sorted = true;
+			if (array[i] > array[i + 1])
+			{
+				is_sorted = false;
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
